8119daef254ad21d7cafdf999dafacf499345360
dashrath11 <122978073+dashrath11@users.noreply.github.com>
2025-02-24 15:34:56 +0530

    Update expno5.py

--- expno5.py
+++ expno5.py
@@ -15,8 +15,8 @@ lr = LinearRegression()
 # Load and split data
 for _ in range(10):
     rng = np.random.RandomState(_)
-    x = 10 * rng.rand(1000).reshape(-1,1)
-    y = 2 * x - 5 + rng.randn(1000).reshape(-1,1)
+    x = 10 * rng.rand(100).reshape(-1,1)
+    y = 2 * x - 5 + rng.randn(100).reshape(-1,1)
 
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=50)
   
